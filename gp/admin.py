533bdb347ff63ba57eac0a2c97ad7718e5514b1b
Edimar Manica <edimarmanica@gmail.com>
2019-10-28 14:00:58 -0300

    Adicionando Regime de Trabalho, Situação Funcional e Área do Concurso

--- gp/admin.py
+++ gp/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
-from gp.models import Titulacao
+from gp.models import Titulacao, RegimeTrabalho, SituacaoFuncional, AreaConcurso
+
+class AreaConcursoAdmin(admin.ModelAdmin):
+    list_display = ('area', 'area_pai', 'nivel')  # definindo o que será exibido na listagem
+    list_filter = ('area_pai', 'nivel')  #definindo os filtros
+    search_fields = ['area', 'area_pai' ]
 
 
 admin.site.register(Titulacao)
+admin.site.register(RegimeTrabalho)
+admin.site.register(SituacaoFuncional)
+admin.site.register(AreaConcurso, AreaConcursoAdmin)
+
--- gp/models.py
+++ gp/models.py
@@ -2,10 +2,49 @@ from django.db import models
 
 # Create your models here.
 class Titulacao(models.Model):
-    descricao = models.CharField(max_length=15)
+    descricao = models.CharField(max_length=15, verbose_name="Descrição")
+    
+    def __str__(self):
+       return self.descricao
     
     class Meta:
         ordering = ["descricao"] # - para ordem decrescente   -- está ordenando nos select e combobox
         verbose_name="Titulação" #nome dos objetos dessa tabela no singular
         verbose_name_plural="Titulações" #nome dos objetos dessa tabela no plural
         
+class RegimeTrabalho(models.Model):
+    descricao = models.CharField(max_length=6, verbose_name="Descrição")
+    
+    def __str__(self):
+       return self.descricao
+       
+    class Meta:
+        ordering = ["descricao"] 
+        verbose_name="Regime de Trabalho" 
+        verbose_name_plural="Regimes de Trabalho"
+        
+class SituacaoFuncional(models.Model):
+    descricao = models.CharField(max_length=15, verbose_name="Descrição")
+    
+    def __str__(self):
+       return self.descricao
+       
+    class Meta:
+        ordering = ["descricao"] 
+        verbose_name="Situação Funcional" 
+        verbose_name_plural="Situações Funcionais"
+        
+
+class AreaConcurso(models.Model):
+    area = models.CharField(max_length=50, verbose_name="Área")
+    area_pai = models.ForeignKey('self', on_delete=models.PROTECT, blank=True, null=True, verbose_name="Área Pai", related_name="pai")
+    nivel = models.IntegerField(verbose_name="Nível")
+    
+    def __str__(self):
+       return self.area
+       
+    class Meta:
+        ordering = ["area"] 
+        verbose_name="Área do Concurso" 
+        verbose_name_plural="Áreas do Concurso"
+ 
